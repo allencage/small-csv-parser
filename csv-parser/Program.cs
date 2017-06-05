d1f90505a523898ba153d2a1685d350e36f39871
allencage <alingabriel.ciuca@gmail.com>
2017-06-05 11:50:21 +0300

    Initial commit

---
+++ csv-parser/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic.FileIO;
+
+namespace csv_parser
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var path = "file.csv";
+			var file = File.OpenRead(path);
+			using(var parser = new TextFieldParser(path))
+			{
+				parser.TextFieldType = FieldType.Delimited;
+				parser.SetDelimiters(",");
+				while (!parser.EndOfData)
+				{
+					var line = parser.ReadFields();
+					foreach (var item in line)
+					{
+						Console.WriteLine(item);
+					}
+				}
+			}
+			Console.Read();
+		}
+	}
+}
